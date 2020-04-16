a4ecbab784a04c1cdfcd7201c99e337e1bcee577
hf <873276172@qq.com>
2020-04-16 21:14:18 +0800

    基础配置

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# blog
-使用vuepress搭建个人博客网站
+### 使用vuepress搭建个人博客网站，[访问路径：https://skycolor.github.io/blog/dist/](https://skycolor.github.io/blog/dist/)
+---
+#### 1、环境
+必须要全局安装vuepress组件
+```
+# 安装，官方建议使用yarn，因为npm容易出现依赖包冲突
+yarn global add vuepress # 或者：npm install -g vuepress
+# 启动
+yarn run dev
+# 打包
+yarn run build
+```
+
+
+#### 2、配置
+- 在config.js中配置base为/blog/dist/，配置dest为docs/dist
+- 在gitpage的setting中，配置master branch /docs folder
+
+
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,19 @@
 module.exports = {
+    // 基础配置
+    title: 'HF\'s blog',
     base: '/blog/dist/',
     dest: 'docs/dist',
-    title: 'Hello VuePress1',
-    description: 'Just playing around'
+    description: '记录一些个人前端文档',
+    // 使用插件
+    plugins: [],
+    // 主题
+    themeConfig: {
+        logo: '/img/logo.jpg',
+        // 导航栏
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    }
 }
