d176779176513216edb048ce273d18dc9da83cd4
hf <873276172@qq.com>
2020-07-20 15:46:25 +0800

    调整博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,8 @@ module.exports = {
         // 导航栏
         nav: [
             { text: '首页', link: '/' },
-            { text: '编程', link: '/programme/' },
+            { text: 'js基础', link: '/javascript/' },
+            { text: '工具', link: '/tool/' },
             { text: '读书笔记', link: '/reading/' },
             { text: '小果冻', link: '/child/' },
             { text: '标签', link: '/tag/' },
@@ -18,9 +19,10 @@ module.exports = {
         ],
         // 文件流
         directories: [
-            { id: 'programme',title: '程序人生', dirname: '_programme', path: '/programme/', },
-            { id: 'reading',title: '隨筆', dirname: '_reading', path: '/reading/', },
-            { id: 'child',title: '小果冻', dirname: '_child', path: '/child/', }
+            { id: 'javascript', title: 'js基础', dirname: '_javascript', path: '/javascript/' },
+            { id: 'tool', title: '工具', dirname: '_tool', path: '/tool/' },
+            { id: 'reading',title: '隨筆', dirname: '_reading', path: '/reading/' },
+            { id: 'child',title: '小果冻', dirname: '_child', path: '/child/' },
         ],
         smoothScroll: true
     }
